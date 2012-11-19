384e268185605977bceb0bec9fce745ffb1ab14b
mcshadow <mcshadow@humanz.biz>
2012-11-19 17:39:49 +0400

    remove - Resultset is null Java null pointer exception

--- scheduler-sql/src/get/get_scheduled_tasks.sql
+++ scheduler-sql/src/get/get_scheduled_tasks.sql
@@ -11,12 +11,10 @@ create procedure get_schedule_tasks(i_max_tasks_count int(10))
       from scheduler_settings
      where setting_name = 'Scheduler paused';
 
-    if v_is_paused = 'N' then
-      select stsk_id task_id, task_name
-        from scheduled_tasks
-       where exec_status in ('C', 'F') and exec_next_date <= v_run_date
-       limit i_max_tasks_count;
-     end if;
+    select stsk_id task_id, task_name
+      from scheduled_tasks
+     where exec_status in ('C', 'F') and exec_next_date <= v_run_date and v_is_paused = 'N'
+     limit i_max_tasks_count;
   end
 $$
 delimiter ;
